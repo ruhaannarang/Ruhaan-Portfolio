d9f1125a59dbb141715a20e4a266aecad7bb0e2c
Ruhaan Narang <techieruhaan@gmail.com>
2025-09-22 16:24:55 +0530

    bug fixes of custom cursor

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let cursor=document.body.querySelector(".cursor")
 let fullScreen=document.body
 fullScreen.addEventListener("mousemove",(dets) => {
-  console.log(dets.x," ",dets.y)
+  // console.log(dets.x," ",dets.y)
   gsap.to(cursor,{
       x:dets.x-12,
       y:dets.y-12
@@ -9,3 +9,37 @@ fullScreen.addEventListener("mousemove",(dets) => {
   })
 }
 )
+
+let projectLinks=document.querySelectorAll(".projectLink")
+projectLinks.forEach(projectLink => {
+  projectLink.addEventListener("mouseover",() => {
+    cursor.style.display="none"
+    document.body.style.cursor = "auto"; // default cursor show karna
+
+});
+}
+)
+projectLinks.forEach(projectLink => {
+  projectLink.addEventListener("mouseout",() => {
+    cursor.style.display="block"
+    document.body.style.cursor = "none"; // default cursor show karna
+
+});
+}
+)
+
+let socialLinks=document.querySelector(".socialLinks")
+
+  socialLinks.addEventListener("mouseover",() => {
+    cursor.style.display="none"
+    document.body.style.cursor = "auto"; // default cursor show karna
+
+});
+
+
+
+  socialLinks.addEventListener("mouseout",() => {
+    cursor.style.display="block"
+    document.body.style.cursor = "none"; // default cursor show karna
+
+});
